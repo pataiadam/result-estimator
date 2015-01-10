cb0354984b2cef130c73f8d00b31d87571dab4e7
pataiadam <pataiadam@gmail.com>
2015-01-10 21:52:30 +0100

    Configure pg database

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # result-estimator
 
-a [Sails](http://sailsjs.org) application
+### Install Postgres database
+
+```
+sudo apt-get install postgresql postgresql-contrib
+```
+
+Set a deffault password for the postgres database:
+```
+sudo -u postgres psql postgres
+\password postgres
+\q
+```
+
+### Set database connections
+```
+sudo -u postgres psql postgres
+create database estimator;
+CREATE USER estimator_user WITH PASSWORD 'kacsa123';
+GRANT ALL PRIVILEGES ON DATABASE "estimator" to estimator_user;
+\q
+```
---
+++ api/controllers/TestController.js
@@ -0,0 +1,22 @@
+/**
+ * TestController
+ *
+ * @description :: Server-side logic for managing tests
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+    index:  function (req, res) {
+        Test.find().exec(function (err, tests) {
+            if (!!err) {
+                sails.log.error(err);
+                req.flash('error', err);
+                return;
+            }
+            Test.create({text: "LOL"}).exec(function (err, test){
+                res.json({test: tests});
+            });
+        });
+    }
+};
+
