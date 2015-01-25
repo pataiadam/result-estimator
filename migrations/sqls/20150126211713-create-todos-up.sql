e1d53f0e77caef36d8bd12af6e645cbdf1954600
gsanta <santagergely90@gmail.com>
2015-01-25 21:29:31 +0100

    Add long and lat to mig

--- migrations/sqls/20150126211713-create-todos-up.sql
+++ migrations/sqls/20150126211713-create-todos-up.sql
@@ -4,6 +4,8 @@ CREATE TABLE public.todos
    title character(32),
    content character(256),
    date timestamp with time zone,
+   longitude decimal,
+   latitude decimal,
    "createdAt" timestamp with time zone,
    "updatedAt" timestamp with time zone,
    CONSTRAINT todos_pkey PRIMARY KEY (todos_id)
